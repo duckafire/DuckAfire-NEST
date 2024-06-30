2910bb6bec52aae4be52212227490b7e71dd0b20
DuckAfire <155199080+duckafire@users.noreply.github.com>
2024-06-29 21:30:18 -0300

    Changes in translation (pt-BR; turned on; based in user browser language)

--- home/lang.js
+++ home/lang.js
@@ -1,74 +1,38 @@
 // en-US by default
 let userLang = navigator.language || navigator.userLanguage;
-let force_enUS = false;
 
-// pt-BR
-if(!force_enUS && userLang.charAt(0) == 'p' && userLang.charAt(1) == "t"){
+// pt-BR ; first is debug
+if(true && userLang.charAt(0) == 'p' && userLang.charAt(1) == "t"){
+	// TEXT
+	let content = [
+		// main (welcome)
+		["tMain",    "Bem vindo ao ninho!"],
+		["pMain-0",  "Olá, eu sou DuckAfire, um desenv. de jogos indie."],
+		["pMain-1",  "Aqui você poderá ver mais sobre o meu trabalho."],
 
-// LINKS TO PARAGRAPHS
-const lnews = [null];
-const lproj = [null, null, null];
+		// news
+		["tNews",   "Notícias"],
+		["pNews-0", "[22.06.2024]"],
+		["pNews-1", "A versão v3.0.0 do"],
+		["pNews-2", "já está disponível!"],
 
-(function(){
-	let max = ((lnews.length > lproj.length) ? lnews.length : lproj.length);
-
-	const text = [
-		["Legendary Champion: Rebirth"],
-		["TinyLibrary", "Tic80", "Legendary Champion: Rebirth"]
+		// noti (notices)
+		["tProj",   "Projetos"],
+		["pProj-0", "Coleção de bibliotecas para o"],
 	];
-	const link = [
-		["github.com/duckafire/TinyLibrary"],
-		["tic80.com", "github.com/duckafire/LIM", "tic80.com/play?cart=3315"]
-	];
-
-	for(let i = 0; i < max; i++){
-		if(i < lnews.length){
-			lnews[i] = document.createElement('a');         // create link element
-			lnews[i].href        = "https://" + link[0][i]; // set link
-			lnews[i].textContent = text[0][i];              // set text
-		}
-		if(i < lproj.length){
-			lproj[i] = document.createElement('a');
-			lproj[i].href        = "https://" + link[1][i];
-			lproj[i].textContent = text[1][i];
-		}
-	}
-})();
-
-// SET PARAGRAPHS CONTENTE (text)
-const texts = [null, null, null, null, null, null];
-	const tags = ["tMain", "pMain", "tNews", "pNoti", "tProj", "pProj"];
-for(let i = 0; i < tags.length; i++) texts[i] = document.getElementById(tags[i]);
-
-(function(){
-	// text link ("a")
-	function texts_linkToObject(id, msg, link){
-		let child = 0;
-		texts[id].textContent = "";
-
-		for(let i = 0; i < msg.length; i++){
-			if(msg.charAt(i) == '\0'){
-				// add link object
-				texts[id].appendChild(link[child]);
-				child++;
-				continue;
-			}
-
-			texts[id].textContent += msg.charAt(i);
-		}
-	}
-
-	// get user browser language
-	texts[0].textContent = "Bem vindo ao meu ninho!";
-	texts[1].textContent = "Olá, eu sou DuckAfire, um desenvolvedor de jogos independente.\nNessa página, você poderá ver mais sobre o meu trabalho.";
-
-	texts[2].textContent = "Notícias";
-	//texts[3].textContent = "<b>[22.06.2024]</b> - A versão v3.0.0 do \n" + lnews[0] + " já está disponível!";
-	texts_linkToObject(3,  "<b>[22.06.2024]</b> - A versão v3.0.0 do \n\0 já está disponível!", lnews);
 	
-	texts[4].textContent = "Projetos";
-	//texts[5].textContent = lproj[0] + " - Coleção de bibliotecas para o " + lproj[1] + "\n" + lproj[2] + " - <b>L</b>ua L<b>i</b>brary Co<b>m</b>pactor\n" + lnews[0] + " - Jogo de console de fantasia";
-	texts_linkToObject(5,  "\0 - Coleção de bibliotecas para o \0\n\0 - <b>L</b>ua L<b>i</b>brary Co<b>m</b>pactor\n" + lnews[0] + " - Jogo de console de fantasia", lproj);
-})();
+	// change content
+	for(let i = 0; i < content.length; i++){
+		document.getElementById(content[i][0]).textContent = content[i][1];
+	}
+
+	// RESIZE
+	let value = [
+		["dMain", "100"],
+	];
+
+	for(let i = 0; i < value.length; i++){
+		document.getElementById(value[i][0]).height = value[i][1] + "px";
+	}
 
 } // END
