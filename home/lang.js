503b0397f972b5e9b7c2e29f7d2ba506d0b71e5c
DuckAfire <155199080+duckafire@users.noreply.github.com>
2024-07-20 12:12:52 -0300

    Updated background, added titles to links and fixed 'fake links'

--- home/lang.js
+++ home/lang.js
@@ -7,27 +7,27 @@ if(true && userLang.charAt(0) == 'p' && userLang.charAt(1) == "t"){
 	let content = [
 		// main (welcome)
 		["tMain",    "Bem vindo ao ninho!"],
-		["pMain-0",  "Olá, eu sou DuckAfire, um desenv. de jogos indie."],
+		["pMain-0",  "Olá, eu sou DuckAfire, um desenvolvedor de jogos indie."],
 		["pMain-1",  "Aqui você poderá ver mais sobre o meu trabalho."],
 
 		// news
 		["tNews",     "Notícias"],
 
-		["pNews-0-0", "--- [13.07.2024] ---"],
+		["pNews-0-0", "13.07"],
 		["pNews-0-1", "LIM atualização v0.1.11:"],
 		["pNews-0-2", "Novo prefixo para adicionar funções à tabela da biblioteca"],
 		["pNews-0-3", "Nomes de funções podem ser protegidos"],
 		["pNews-0-4", "Funções da biblioteca podem ser usada dentro da mesma"],
-		["pNews-0-5", "E MAIS! "],
+		["pNews-0-5", "E MAIS!"],
 
-		["pNews-1-0", "--- [09.07.2024] ---"],
+		["pNews-1-0", "09.07"],
 		["pNews-1-1", "Legendary Champion atualização v3.0.1:"],
 		["pNews-1-2", "Efeitos sonoros"],
 		["pNews-1-3", "Ajustes nos mapas"],
 		["pNews-1-4", "Várias correções"],
-		["pNews-1-5", "E MAIS! "],
+		["pNews-1-5", "E MAIS!"],
 
-		["pNews-2-0", "--- [22.06.2024] ---"],
+		["pNews-2-0", "22.06"],
 		["pNews-2-1", "A versão v3.0.0 do "],
 		["pNews-2-2", "já está disponível!"],
 
