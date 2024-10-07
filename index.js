9df0020e00278363e5241ed80450dbb5542387a2
DuckAfire <155199080+duckafire@users.noreply.github.com>
2024-10-07 10:50:05 -0300

    New text font + panel to social medias + hyperlinks + other little somethings

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const SocialMedia = {
 	id: 0,
-	opened: false,
 	icon: document.getElementById("social-media"),
+	iconBg: document.getElementById("social-panel-background"),
 	classes: ["github", "itch-io", "facebook", "gamepad"],
 	anim: function(it){
-		if(it.opened) return;
-		
 		let group = "brands";
 		if(it.id == 3) group = "solid";
 
-		it.icon.className = "fa-" + group + " fa-" + it.classes[it.id];
+		it.icon.className = "cursor-hand fa-" + group + " fa-" + it.classes[it.id];
 		it.id++;
 		if(it.id > 3) it.id = 0;
 	},
-	/*panel: function(){
-		const pos = this.getBoundingClientRect();
-		const panel = document.getElementById("social-media-panel");
-		panel.hidden = false;
-		panel.style.top  = "8vh";
-		panel.style.left = pos.y + "px";//(Number(pos.y) + 1).toStrin() + "px";
-	},*/
 };
 
 setInterval(SocialMedia.anim, 2500, SocialMedia);
-//SocialMedia.icon.addEventListener("click", SocialMedia.panel);
+SocialMedia.icon.addEventListener("click", () => {SocialMedia.iconBg.hidden = false;})
+SocialMedia.iconBg.addEventListener("click", () => {SocialMedia.iconBg.hidden = true;});
+document.getElementById("social-panel-shape").addEventListener("click", (event) => {event.stopPropagation();});
+document.getElementById("donate").addEventListener("click", () => {window.open("https://github.com/sponsors/duckafire");});
