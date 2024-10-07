43bae2dfe55533328e108b858f7b84c53c313cac
DuckAfire <155199080+duckafire@users.noreply.github.com>
2024-10-07 18:00:02 -0300

    Swap language + English version (not revised) + links to projects + small improves

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const PageValues = {
 	language: (navigator.userLanguage || navigator.language).slice(0, 2) == "pt",
 	lightScheme: window.matchMedia("(prefers-color-scheme: light)").matches,
+	setLanguage: function(){
+		PageValues.language = !PageValues.language;
+
+		if(PageValues.language){
+			LanguagePT(TextContent);
+			return;
+		}
+
+		LanguageEN(TextContent)
+	},
 };
 
 if(PageValues.language){
@@ -11,6 +21,7 @@ if(PageValues.language){
 
 const BurgerMenu = document.getElementById("burger-menu");
 const Donate     = document.getElementById("donate");
+const GlobeLang  = document.getElementById("language");
 
 const SocialMediaPanel = {
 	bg:    document.getElementById("social-panel-background"),
@@ -44,9 +55,16 @@ Donate.addEventListener("click", () => {window.open("https://github.com/sponsors
 window.setInterval(SocialMediaPanel.iconAnim, 2500, SocialMediaPanel);
 SocialMediaPanel.bg.addEventListener(   "click", () => {SocialMediaPanel.bg.hidden = true; });
 SocialMediaPanel.shape.addEventListener("click", (event) => {event.stopPropagation();});
-SocialMediaPanel.icon.addEventListener( "click", () => {SocialMediaPanel.bg.hidden = false;})
+SocialMediaPanel.icon.addEventListener( "click", () => {
+	SocialMediaPanel.bg.hidden = !SocialMediaPanel.bg.hidden;
+	for(let i = 0; i < 3; i++)
+		DrawerContent[i].children[1].hidden = true;
+})
 
-for(let i = 0; i < 3; i++)
+for(let i = 0; i < 3; i++){
 	DrawerContent[i].children[0].addEventListener("click", (event) => {
 		event.currentTarget.parentNode.children[1].hidden = !event.currentTarget.parentNode.children[1].hidden;
 	});
+}
+
+GlobeLang.addEventListener("click", PageValues.setLanguage);
