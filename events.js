d40cae6d33f99bb55494b160bc2f1e372c68295d
DuckAfire <155199080+duckafire@users.noreply.github.com>
2024-10-11 08:30:23 -0300

    Color effect + trash cleaned

--- events.js
+++ events.js
@@ -14,24 +14,49 @@ for(let i = 0; i < 3; i++){
 
 const PerfilIcon = document.getElementById("co").children[0];
 PerfilIcon.addEventListener("click", () => {
-	/*
-	let variables = [
-		"panel",
-		"sub-panel",
-	];
+	let panel = ["panel", "sub-panel"];
 	
-	let root = getComputedStyle(document.documentElement);
-	let value = "";
+	let rootSet = document.documentElement;
+	let rootGet = getComputedStyle(rootSet);
+	let red = 0, green = 0, blue = 0;
+	let all = "";
 
-	for(let i = 0; i < variables.length; i++){
-		value = (root.getPropertyValue("--color-" + variables[i])).slice(1, 3);
-		value = parseInt(value, 16) + 16;
-
-		if(value - 16 > 255) continue;
-		if(value > 255) value = 255;
-
-		console.log("--color-" + variables[i]);
-		root.setProperty("--color-" + variables[i], "#" + value.toString(16));
+	function getRGB(){
+		red   = parseInt(all.slice(1, 3), 16);
+		green = parseInt(all.slice(3, 5), 16);
+		blue  = parseInt(all.slice(5), 16);
 	}
-	*/
+
+	for(let i = 0; i < 2; i++){
+		all = rootGet.getPropertyValue("--color-" + panel[i]);
+		if((i == 0 && all == "#df4049") || (i == 1 && all == "#af3017")) continue;
+		getRGB()
+
+		red   = red   + 4;
+		green = green - 2;
+		blue  = blue  - 1;
+
+		if(red   > 255) red   = 255;
+		if(green < 048) green = 048;
+		if(blue  < 032) blue  = 032;
+
+		all = "#" + red.toString(16) + green.toString(16) + all.slice(5);
+		rootSet.style.setProperty("--color-" + panel[i], all);
+	}
+
+	all = rootGet.getPropertyValue("--color-link");
+	if(all != "#8dff30"){
+		getRGB();
+
+		red   = red   - 2;
+		green = green + 3;
+		blue  = blue  - 1;
+
+		if(red   < 064) red   = 064;
+		if(green > 255) green = 255;
+		if(blue  < 048) blue  = 048;
+
+		all = "#" + red.toString(16) + green.toString(16) + blue.toString(16);
+		rootSet.style.setProperty("--color-link", all);
+	}
 });
