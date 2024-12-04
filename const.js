cf2bc9b47fed3635f10413fa61147734d2485017
DuckAfire <155199080+duckafire@users.noreply.github.com>
2024-12-04 09:58:20 -0300

    Added languages used in 'project-box' and added x2 projects

--- const.js
+++ const.js
@@ -2,3 +2,4 @@ const PROJECT_BODY            = document.getElementById("projects-main");
 const PROJECT_BODY_MORE       = document.getElementById("projects-more");
 const PROJECT_BODY_BOX_CLASS  = "project-box";
 const PROJECT_BODY_LINK_CLASS = "project-link";
+const PROJECT_BODY_LANG_CLASS = "project-used-languages";
--- dom.js
+++ dom.js
@@ -1,18 +1,37 @@
-function newProjectPanel(_img, _title, _descr, links){
+function newProjectPanel(_img, _title, _descr, lang, links){
 	// containers
-	let fullCtt = document.createElement("div");
-	let textCtt = document.createElement("div");
-	let linkCtt = document.createElement("div");
+	const fullCtt = document.createElement("div");
+	const textCtt = document.createElement("div");
+	const linkCtt = document.createElement("div");
+	const langCtt = [document.createElement("div"), document.createElement("div")];
+
+	// <a> <i> <span>
+	let a, i, s;
 
 	fullCtt.className = PROJECT_BODY_BOX_CLASS;
 	linkCtt.className = PROJECT_BODY_LINK_CLASS;
 
 	// panel elements (except links)
-	let img   = document.createElement("img");
-	let title = document.createElement("h2");
-	let descr = document.createElement("p");
+	const img   = document.createElement("img");
+	const title = document.createElement("h2");
+	const descr = document.createElement("p");
 
-	img.src = "./img/project-" + _img + ".png";
+	// project icon/cover
+	img.src = "./img/project-" + _img + "-icon.png";
+
+	// languages used in project
+	langCtt[0].id = PROJECT_BODY_LANG_CLASS;
+	langCtt[1].title = "Linguagens usadas"
+
+	for(let j = 0; j < lang.length; j++){
+		i = document.createElement("i");
+
+		i.className = "devicon-" + lang[j] + "-plain";
+
+		langCtt[1].appendChild(i);
+	}
+
+	langCtt[0].appendChild(langCtt[1]);
 
 	// text content to sub-container
 	title.textContent = _title;
@@ -22,7 +41,6 @@ function newProjectPanel(_img, _title, _descr, links){
 	textCtt.appendChild(descr);
 
 	// links content to sub-container
-	let a, i, s;
 	for(let j = 0; j < links.length; j++){
 		a = document.createElement("a");
 		i = document.createElement("i");
@@ -40,6 +58,7 @@ function newProjectPanel(_img, _title, _descr, links){
 
 	// add elements to mother container
 	fullCtt.appendChild(img);
+	fullCtt.appendChild(langCtt[0]);
 	fullCtt.appendChild(textCtt);
 	fullCtt.appendChild(linkCtt);
 
@@ -53,9 +72,9 @@ function newProjectPanel(_img, _title, _descr, links){
 }
 
 function createProjectsMoreButton(){
-	let div0 = document.createElement("div");
-	let div1 = document.createElement("div");
-	let i    = document.createElement("i");
+	const div0 = document.createElement("div");
+	const div1 = document.createElement("div");
+	const i    = document.createElement("i");
 
 	div0.id = "projects-more-icon";
 	div0.addEventListener("click", showMoreProjects);
